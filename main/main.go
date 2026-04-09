c3f726cbb3fdc649b1581a8d54f1b1c56682748f
Gauri Lokhande <157399853+lokhandegauri123@users.noreply.github.com>
2026-04-09 12:54:46 +0530

    new code

--- main/main.go
+++ main/main.go
@@ -1,5 +1,15 @@
 package main
 import  "fmt"
+
+func fact_recursion(x int) (res int){
+	if x > 0{
+		res = x * fact_recursion(x -1)
+	} else{
+		res = 1
+	}
+	return
+}
+
 func main(){
-	fmt.Println("hello")
+	fmt.Println(fact_recursion(4))
 }
