ca2fbe451c3c701aa24c6c618236e54a78e1df8b
Gauri Lokhande <157399853+lokhandegauri123@users.noreply.github.com>
2026-04-13 17:51:57 +0530

    learned Buffered Channel concept

--- Channel/channel.go
+++ Channel/channel.go
@@ -1,4 +1,4 @@
-package main
+/* package main
 
 import (
     "fmt"
@@ -25,4 +25,32 @@ func main() {
     case msg2 := <-ch2:
         fmt.Println(msg2)
     }
+} */
+
+// Buffered Channel
+/* A buffered channel is a channel that can store multiple values before they are received.
+
+It allows sending data without immediate receiver
+Real-life Analogy
+
+Think of a queue (line) 🧾:
+
+People can stand in line (buffer)
+You don’t need to serve immediately
+
+Important Behavior ⚠️
+✅ Send does NOT block (until buffer full) */
+
+package main
+
+import "fmt"
+
+func main() {
+    ch := make(chan int, 2)
+
+    ch <- 10
+    ch <- 20
+
+    fmt.Println(<-ch)
+    fmt.Println(<-ch)
 }
